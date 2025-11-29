186d6ff82f00e2305d91aa29059cbabe6ef05838
nandinichintewad <2024bit031@sggs.ac.in>
2025-11-29 11:15:03 +0530

    add code

---
+++ loop.java
@@ -0,0 +1,13 @@
+public class loop {
+    public static void main (String[]args){
+    
+    int table;
+    for(int sum=2;sum<=10;sum++){
+    {for(int i=1;i<=10;i++){
+     table=sum*i;
+     System.out.print(table+ " ");
+    }System.out.println();
+    }
+}
+}
+}
