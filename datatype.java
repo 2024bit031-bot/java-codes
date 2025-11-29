1c00fe4b40af38848d806dcb9f27737c6a35aefb
nandinichintewad <2024bit031@sggs.ac.in>
2025-11-29 10:12:58 +0530

    first commit

---
+++ datatype.java
@@ -0,0 +1,14 @@
+public class datatype {
+    public static void main(String[]args)
+    {
+      int i=10;
+      String ch="i am new devloper in java";
+      System.out.println(i);
+      System.out.println(ch);
+
+
+
+
+    }
+    
+}
