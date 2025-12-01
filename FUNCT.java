a22b177d03f74b2b681b323cee6802f2ddf7decb
nandinichintewad <2024bit031@sggs.ac.in>
2025-12-01 22:39:04 +0530

    add code3

---
+++ FUNCT.java
@@ -0,0 +1,17 @@
+public class FUNCT {
+
+public static void school(){
+System.out.println("its my schooln relashionship");
+}
+public static void student(){
+System.out.println("shravani");
+}
+public static void name(){
+System.out.println("vaibhav");
+}
+    public static void main(String[]args){
+school();
+student();
+name();
+    }
+}
